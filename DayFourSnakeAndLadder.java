3baae86ae2f8562606b9b32353dff521f106c2e7
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-28 15:16:35 +0530

    first commit Both Player At 0

---
+++ DayFourSnakeAndLadder.java
@@ -0,0 +1,15 @@
+
+public class DayFourSnakeAndLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class snakeLadderGame {
+	void gameToBeStarted() {
+		int Pos = 0;
+	}
+}
