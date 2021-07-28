fd49f3042de101797378b60e30352d1dd3c3564b
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-28 16:15:07 +0530

    Second Commit Dice Roll.

--- DayFourSnakeAndLadder.java
+++ DayFourSnakeAndLadder.java
@@ -1,11 +1,24 @@
+import java.util.Random;
 
 public class DayFourSnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int Dice=0;
+		RollADice();
+		
 
 	}
 
+	
+	public static int RollADice()
+	{
+		Random dice=new Random();
+		int Number=dice.nextInt(6)+1;
+		System.out.println(Number);
+		return Number;
+	}
 }
 
 class snakeLadderGame {
