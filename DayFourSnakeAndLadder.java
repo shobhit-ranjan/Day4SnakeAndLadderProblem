8a4f716f732d25a6c554829ecd0954b5cc89abb9
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-28 16:29:15 +0530

    Third Commit Play or Noplay.

--- DayFourSnakeAndLadder.java
+++ DayFourSnakeAndLadder.java
@@ -4,21 +4,40 @@ public class DayFourSnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int Dice=0;
+
+		int Dice = 0;
 		RollADice();
-		
+		playOrNoPlay();
 
 	}
 
-	
-	public static int RollADice()
+	public static int playOrNoPlay()
 	{
-		Random dice=new Random();
-		int Number=dice.nextInt(6)+1;
+		int chance = 0;
+		Random player = new Random();
+		int temp = player.nextInt(3);
+		if(temp==0)
+		{
+			chance=0;
+		}
+		else if (temp==1) {
+			chance=1;
+			
+		}
+		else {
+			chance=-1;
+		}
+		System.out.println(chance);
+		return chance;
+	}
+
+	public static int RollADice() {
+		Random dice = new Random();
+		int Number = dice.nextInt(6) + 1;
 		System.out.println(Number);
 		return Number;
 	}
+
 }
 
 class snakeLadderGame {
